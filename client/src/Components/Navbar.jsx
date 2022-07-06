44c7a30e1c6dbda3163afe6be320f6d1e54e9d6a
diegop1zarro <diego.cab.1016@gmail.com>
2022-07-06 20:37:43 -0300

    Navbar diego y images

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -1,122 +1,79 @@
-import React, { useState } from 'react';
-import { createStyles, Header, Group, ActionIcon, Container, Burger } from '@mantine/core';
-import { useBooleanToggle } from '@mantine/hooks';
-import { BrandTwitter, BrandYoutube, BrandInstagram } from 'tabler-icons-react';
-import { MantineLogo } from '../../shared/MantineLogo';
+import React from "react";
+import {
+  createStyles,
+  Header,
+  Group,
+  Container,
+  Button,
+} from "@mantine/core";
+import LOGOnombre from '../images/LOGOnombre.jpg';
 
-// const useStyles = createStyles((theme) => ({
-//   inner: {
-//     display: 'flex',
-//     justifyContent: 'space-between',
-//     alignItems: 'center',
-//     height: 56,
+const useStyles = createStyles((theme) => ({
+  header: {
+    backgroundColor: "#1bff72",
+  },
+  inner: {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    height: 56,
 
-//     [theme.fn.smallerThan('sm')]: {
-//       justifyContent: 'flex-start',
-//     },
-//   },
+    [theme.fn.smallerThan("sm")]: {
+      justifyContent: "flex-start",
+    },
+  },
 
-//   links: {
-//     width: 260,
+  links: {
+    width: 260,
 
-//     [theme.fn.smallerThan('sm')]: {
-//       display: 'none',
-//     },
-//   },
+    [theme.fn.smallerThan("sm")]: {
+      display: "none",
+    },
+  },
 
-//   social: {
-//     width: 260,
+  social: {
+    width: 260,
 
-//     [theme.fn.smallerThan('sm')]: {
-//       width: 'auto',
-//       marginLeft: 'auto',
-//     },
-//   },
+    [theme.fn.smallerThan("sm")]: {
+      width: "auto",
+      marginLeft: "auto",
+    },
+  },
+}));
 
-//   burger: {
-//     marginRight: theme.spacing.md,
+export default function Navbar() {
+  const { classes } = useStyles();
 
-//     [theme.fn.largerThan('sm')]: {
-//       display: 'none',
-//     },
-//   },
+  return (
+    <Header className={classes.header} height={56} mb={120}>
+      <Container className={classes.inner}>
+      <Group position="left" noWrap>
+      <a href='#'>
+         <Button variant="outline" color="dark" compact uppercase>
+           PLANTILLAS
+         </Button>
+         </a>
+        <a href='#'>
+         <Button variant="outline" color="dark" compact uppercase>
+           MAS INFO
+         </Button>
+         </a>
+      </Group> 
+        <img className={classes.inner} src={LOGOnombre} alt="logo" />
+        <Group position="right" noWrap>
+        <a href='#'>
+          <Button variant="outline" color="dark" compact uppercase>
+            QUIERO EMPEZAR
+          </Button>
+          </a>
+          <a href='#'>
+          <Button variant="outline" color="dark" compact uppercase>
+            INICIAR SESION
+          </Button>
+          </a>
 
-//   link: {
-//     display: 'block',
-//     lineHeight: 1,
-//     padding: '8px 12px',
-//     borderRadius: theme.radius.sm,
-//     textDecoration: 'none',
-//     color: theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.colors.gray[7],
-//     fontSize: theme.fontSizes.sm,
-//     fontWeight: 500,
-
-//     '&:hover': {
-//       backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
-//     },
-//   },
-
-//   linkActive: {
-//     '&, &:hover': {
-//       backgroundColor:
-//         theme.colorScheme === 'dark'
-//           ? theme.fn.rgba(theme.colors[theme.primaryColor][9], 0.25)
-//           : theme.colors[theme.primaryColor][0],
-//       color: theme.colors[theme.primaryColor][theme.colorScheme === 'dark' ? 3 : 7],
-//     },
-//   },
-// }));
-
-// interface HeaderMiddleProps {
-//   links: { link: string; label: string }[];
-// }
-
-// export function HeaderMiddle({ links }: HeaderMiddleProps) {
-//   const [opened, toggleOpened] = useBooleanToggle(false);
-//   const [active, setActive] = useState(links[0].link);
-//   const { classes, cx } = useStyles();
-
-//   const items = links.map((link) => (
-//     <a
-//       key={link.label}
-//       href={link.link}
-//       className={cx(classes.link, { [classes.linkActive]: active === link.link })}
-//       onClick={(event) => {
-//         event.preventDefault();
-//         setActive(link.link);
-//       }}
-//     >
-//       {link.label}
-//     </a>
-//   ));
-
-//   return (
-//     <Header height={56} mb={120}>
-//       <Container className={classes.inner}>
-//         <Burger
-//           opened={opened}
-//           onClick={() => toggleOpened()}
-//           size="sm"
-//           className={classes.burger}
-//         />
-//         <Group className={classes.links} spacing={5}>
-//           {items}
-//         </Group>
-
-//         <MantineLogo />
-
-//         <Group spacing={0} className={classes.social} position="right" noWrap>
-//           <ActionIcon size="lg">
-//             <BrandTwitter size={18} />
-//           </ActionIcon>
-//           <ActionIcon size="lg">
-//             <BrandYoutube size={18} />
-//           </ActionIcon>
-//           <ActionIcon size="lg">
-//             <BrandInstagram size={18} />
-//           </ActionIcon>
-//         </Group>
-//       </Container>
-//     </Header>
-//   );
-// }
+        </Group>
+      </Container>
+    </Header>
+  );
+}
