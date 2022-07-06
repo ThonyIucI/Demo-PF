6510db7b46bed2b15a55b5891312043e6de5b76f
ThonyIucI <anthonyau011798@gmail.com>
2022-07-06 09:36:50 -0500

    nuevas rutas de usuario y edit
    instalacion de React router dom 5

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,17 @@
 import React from "react";
 import Home from "./components/Home/HomeContainer/HomeContainer";
 
+import { BrowserRouter, Route } from "react-router-dom";
+import USerPanel from "./components/UserPanel/UserPanel";
+import EditPanel from "./components/EditPanel/EditPanel";
 function App() {
-  return <Home />;
+  return (
+    <BrowserRouter>
+      <Route exact path="/" component={Home} />
+      <Route path="/userpanel" component={USerPanel} />
+      <Route path="/editpanel" component={EditPanel} />
+    </BrowserRouter>
+  );
 }
 
 export default App;
