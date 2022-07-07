8b06cbf3d010a5cfaf8502b3419ead7966b353e3
Dv-del <alexis1614@live.com.ar>
2022-07-07 14:08:12 -0300

    order routes

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,35 +1,8 @@
 const { Router } = require('express')
 
-const { modelTemplate } = require('../db.js')
+const { getTemplates, createTemplate } = require('../controllers/templates.js')
 
 const routes = Router()
-
-const getTemplates = async (req, res) => {
-    try {
-        const response = await modelTemplate.findAll({raw: true})
-        res.status(200).json(response)
-    } catch (error) {
-        res.status(500).send({msg: 'Error interno del servidor', error})
-    }
-}
-
-const createTemplate = async (req, res) => {
-    const { name, skin, image, description, rating } = req.body
-    
-    try {
-        const template = await modelTemplate.create({
-            name,
-            skin,
-            image,
-            description,
-            rating
-        })
-
-        res.status(200).send({msg: 'Template creado!'})
-    } catch (error) {
-        res.status(500).send({msg: 'Error interno del servidor', error})
-    }
-}
 
 routes.get('/templates', getTemplates)
 routes.post('/templates', createTemplate)
