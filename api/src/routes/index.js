aae2874ebeba39454bd74110abd8acad4f7adae8
Dv-del <alexis1614@live.com.ar>
2022-07-07 14:17:19 -0300

    fix

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -2,7 +2,7 @@ const { Router } = require('express')
 
 const { getTemplates, createTemplate } = require('../controllers/templates.js')
 
-const routes = Router()
+const routes = Router() 
 
 routes.get('/templates', getTemplates)
 routes.post('/templates', createTemplate)
